22c0acec059a10cc209fb8cc6908ba3494475a1f
lordimpi <snt-26@hotmail.com>
2020-04-15 01:41:14 -0500

    Create and Modify DB

---
+++ MyVet.Web/Data/Entities/Agenda.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyVet.Web.Data.Entities
+{
+    public class Agenda
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Date")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd H:mm tt}", ApplyFormatInEditMode = true)]
+        public DateTime Date { get; set; }
+
+        public string Remarks { get; set; }
+
+        [Display(Name = "Is Available?")]
+        public bool IsAvailable { get; set; }
+
+        public Owner Owner { get; set; }
+
+        public Pet Pet { get; set; }
+    }
+}
