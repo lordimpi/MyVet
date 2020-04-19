b5c02e5ba4ee7edf5ee2f4e7ef9791e3dce06e55
lordimpi <snt-26@hotmail.com>
2020-04-18 19:03:18 -0500

    Add Token to API

--- MyVet.Web/Controllers/API/PetTypesController.cs
+++ MyVet.Web/Controllers/API/PetTypesController.cs
@@ -1,17 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using MyVet.Web.Data;
 using MyVet.Web.Data.Entities;
+using System.Collections.Generic;
 
 namespace MyVet.Web.Controllers.API
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class PetTypesController : ControllerBase
     {
         private readonly DataContext _context;
